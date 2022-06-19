8bc5e9983c55f63fa2590ef982f97de8e9926113
Ulan <ulanbekenov555@gmail.com>
2022-06-19 23:49:13 +0600

    Добавил mockApi и карточки, корзину связал с ним

--- src/App.js
+++ src/App.js
@@ -1,40 +1,71 @@
+import React from 'react';
+import axios from 'axios';
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 import Card from "./components/Card";
 
-let arr = [{
-  title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-  imageUrl: '/img/sneakers/1.jpg',
-  price: 12500
-},{
-  title: 'Мужские Кроссовки Adidas Suede',
-  imageUrl: '/img/sneakers/2.jpg',
-  price: 32200
-},]
+
 
 function App() {
+
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+   axios.get('https://62ad359e402135c7acbdd301.mockapi.io/Items').then((res) => 
+   setItems(res.data));
+   axios.get('https://62ad359e402135c7acbdd301.mockapi.io/Cart').then((res) => 
+   setCartItems(res.data));
+  }, []);
+
+  const onAddToCart = (obj) => {
+    axios.post('https://62ad359e402135c7acbdd301.mockapi.io/Cart', obj);
+    setCartItems(prev => [...prev, obj])
+  }
+
+  const onRemoveCart = (id) => {
+    axios.delete(`https://62ad359e402135c7acbdd301.mockapi.io/Cart/${id}`);
+     setCartItems(prev => prev.filter(item => item.id !== id))
+  }
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove = {onRemoveCart}/>}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : "Все кроссовки"}</h1>
           <div className="search-block d-flex">
-            <img src="/img/search.svg" alt="Search"/>
-            <input placeholder="Поиск ..."/>
+            <img src="/img/search.svg" alt="Search" />
+            {searchValue && <img
+              onClick={() => { setSearchValue('') }}
+              className="clear removeBtn cu-p"
+              src="/img/btn-remove.svg"
+              alt="Clear" />}
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск ..." />
           </div>
         </div>
-
-      <div className="d-flex">
-        {arr.map((obj) => {
-         return <Card 
-            title = {obj.title}
-            imageUrl = {obj.imageUrl}
-            price = {obj.price}
-          />
-        }) }
-      </div>
+       
+        <div className="d-flex flex-wrap">
+          {items
+            .filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+            .map((item, index) => {
+              return <Card
+                key={index}
+                title={item.title}
+                imageUrl={item.imageUrl}
+                price={item.price}
+                onFavorite={() => console.log("fav")}
+                onPlus={(obj) => onAddToCart(obj)}
+              />
+            })}
+        </div>
       </div>
     </div>
   );
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,28 @@
 import styles from './Card.module.scss'
+import React from 'react';
 
-function Card(props) {
+function Card({ title, imageUrl, price, onFavorite, onPlus}) {
    
+    const [isAdded, setAdded] = React.useState(false);
+
+    const onClickPlus = () => {
+        onPlus({title, imageUrl, price})
+        setAdded(!isAdded)
+    }
+
     return (
         <div className={styles.card}>
-            <div className={styles.favorite}>
+            <div className={styles.favorite} onClick={onFavorite}>
             <img src="/img/heart-unliked.svg" alt="Unliked" />  
             </div>
-            <img width={133} height={112} src={props.imageUrl}/>
-            <h5>{props.title}</h5>
+            <img width={133} height={112} src={imageUrl}/>
+            <h5>{title}</h5>
             <div className="d-flex justify-between align-center"> 
                 <div className="d-flex flex-column">
                 <span>Цена:</span>
-                <b>{props.price} руб.</b>
+                <b>{price} руб.</b>
                 </div>
-                <button className="button" >
-                    <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
-                </button>
+                <img className={styles.plus} onClick={onClickPlus} src={isAdded ? "/img/btn-cheked.svg" : "/img/plus.svg"} alt="Plus"/>
             </div>
         </div>
     )
